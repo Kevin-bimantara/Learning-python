bd4e1ffa3a04b63d3d1f4801c0625cd7c45ff020
Kevin-bimantara <kevinwahyub@gmail.com>
2026-06-04 11:12:17 +0700

    pesan

--- learning python/learning python.py
+++ learning python/learning python.py
@@ -1,5 +1,12 @@
 # Ini adalah contoh program sederhana (teks dengan tanda pagar adalah komentar. 
 # tidak dibaca oleh komputer)
+#Aturan Penamaan Variabel di Python:
+#Hanya boleh berisi huruf, angka, dan underscore (_).
+#Tidak boleh diawali dengan angka (contoh: 1koin itu salah, koin1 itu benar).
+#Tidak boleh ada spasi (gunakan underscore sebagai pemisah, misalnya harga_koin).
+#Membedakan huruf besar dan kecil (case-sensitive). Umur dan umur dianggap sebagai 
+# dua kotak yang berbeda.
+#tipe data dasar
 aset = "Solana"             # Tipe data String
 jumlah_koin = 10            # Tipe data Integer
 harga_beli = 145.75         # Tipe data Float
