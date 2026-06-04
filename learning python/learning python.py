d532e7a709f65fd2d77af9028d84be3d38710976
Kevin-bimantara <kevinwahyub@gmail.com>
2026-06-04 10:59:03 +0700

    pesan

---
+++ learning python/learning python.py
@@ -0,0 +1,12 @@
+# Ini adalah contoh program sederhana (teks dengan tanda pagar adalah komentar, tidak dibaca oleh komputer)
+
+aset = "Solana"             # Tipe data String
+jumlah_koin = 10            # Tipe data Integer
+harga_beli = 145.75         # Tipe data Float
+sedang_ditahan = True       # Tipe data Boolean
+
+# Menampilkan isi variabel ke layar
+print("Nama Aset:", aset)
+print("Jumlah:", jumlah_koin)
+print("Harga Masuk:", harga_beli)
+print("Masih Dipegang?", sedang_ditahan)
