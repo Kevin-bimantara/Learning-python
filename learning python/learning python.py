7e8f40e0e62c6ee124cf2cac382a8992366f303f
Kevin-bimantara <kevinwahyub@gmail.com>
2026-06-04 11:06:30 +0700

    pesan

--- learning python/learning python.py
+++ learning python/learning python.py
@@ -1,5 +1,5 @@
-# Ini adalah contoh program sederhana (teks dengan tanda pagar adalah komentar, tidak dibaca oleh komputer)
-
+# Ini adalah contoh program sederhana (teks dengan tanda pagar adalah komentar. 
+# tidak dibaca oleh komputer)
 aset = "Solana"             # Tipe data String
 jumlah_koin = 10            # Tipe data Integer
 harga_beli = 145.75         # Tipe data Float
@@ -10,3 +10,5 @@ print("Nama Aset:", aset)
 print("Jumlah:", jumlah_koin)
 print("Harga Masuk:", harga_beli)
 print("Masih Dipegang?", sedang_ditahan)
+#Kalau kamu bingung suatu variabel itu tipe datanya apa, Python punya alat bernama type().
+# Contoh: print(type(aset)) nanti akan memunculkan tulisan <class 'str'> yang artinya String.
